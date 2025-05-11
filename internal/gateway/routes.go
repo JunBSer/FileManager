09933eaa6a7d1c9ea37a5dd2cd6a75d83c984ab6
JunBSer <lesharad2006@gmail.com>
2025-05-11 19:20:16 +0300

    fix: fixed pipline

--- internal/gateway/routes.go
+++ internal/gateway/routes.go
@@ -2,7 +2,6 @@ package gateway
 
 import (
 	"context"
-	"github.com/JunBSer/FileManager/pkg/logger"
 	"github.com/gorilla/mux"
 	httpSwagger "github.com/swaggo/http-swagger"
 	"net/http"
@@ -12,11 +11,9 @@ func (h Handler) SetupRoutes(ctx context.Context, r *mux.Router) {
 	r.PathPrefix("/swagger/").Handler(httpSwagger.WrapHandler)
 
 	filesRouter := r.PathPrefix("/api/v1/files").Subrouter()
-	filesRouter.Use(CorsMiddleware, LoggerMiddleware(logger.GetLoggerFromContext(ctx)))
-
 	filesRouter.HandleFunc("/upload", h.Upload).Methods("POST")
-	filesRouter.Handle("/download", TransportFromServMiddleware(http.HandlerFunc(h.Download))).Methods("GET")
-	filesRouter.Handle("/read", TransportFromServMiddleware(http.HandlerFunc(h.Read))).Methods("GET")
+	filesRouter.Handle("/download", http.HandlerFunc(h.Download)).Methods("GET")
+	filesRouter.Handle("/read", http.HandlerFunc(h.Read)).Methods("GET")
 	filesRouter.HandleFunc("/append", h.Append).Methods("POST")
 	filesRouter.HandleFunc("/overwrite", h.Overwrite).Methods("PUT")
 	filesRouter.HandleFunc("/delete", h.Delete).Methods("DELETE")
